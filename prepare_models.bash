60188fd20e987f1bb3de47445341460e5f3f683e
JavohirJalilov <javohirjaliloff@gmail.com>
2023-12-16 10:10:20 +0500

    fix

--- prepare_models.bash
+++ prepare_models.bash
@@ -10,22 +10,22 @@ sudo CUDA_VER=11.4 make -C nvdsinfer_custom_impl_Yolo
 cd -
 
 if [ ! config_infer_primary_yoloV5.txt ]; then
-    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/config_infer_primary_yoloV5.txt $PWD
+    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/config_infer_primary_yoloV5.txt $PWD/
 else
     echo "config_infer_primary_yoloV5.txt already exists!"
 
 if [ ! yolov5s.wts ]; then
-    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/yolov5s.wts $PWD
+    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/yolov5s.wts $PWD/
 else
     echo "yolov5s.wts already exists!"
 
 if [ ! libnvdsinfer_custom_impl_Yolo.so ]; then
-    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/libnvdsinfer_custom_impl_Yolo.so $PWD
+    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/libnvdsinfer_custom_impl_Yolo.so $PWD/
 else
     echo "libnvdsinfer_custom_impl_Yolo.so already exists!"
 
 if [ ! labels.txt ]; then
-    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/labels.txt $PWD
+    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/labels.txt $PWD/
 else
     echo "labels.txt already exists!"
 
