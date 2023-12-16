d66f53dd6a644489a9d4bbaf8e7ce9155dc0f53f
JavohirJalilov <javohirjaliloff@gmail.com>
2023-12-16 10:43:05 +0500

    fixed

--- prepare_models.bash
+++ prepare_models.bash
@@ -9,16 +9,14 @@ sudo CUDA_VER=11.4 make -C nvdsinfer_custom_impl_Yolo
 
 cd -
 
-sudo apt-get install dos2unix
-
 if [ ! -e config_infer_primary_yoloV5.txt ]; then
-    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/config_infer_primary_yoloV5.txt $PWD/
+    cp $home_dir/DeepStream-Yolo/config_infer_primary_yoloV5.txt $PWD/
 else
     echo "config_infer_primary_yoloV5.txt already exists!"
 fi
 
 if [ ! -e yolov5s.wts ]; then
-    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/yolov5s.wts $PWD/
+    cp $home_dir/DeepStream-Yolo/yolov5s.wts $PWD/
 else
     echo "yolov5s.wts already exists!"
 fi 
@@ -30,13 +28,13 @@ else
 fi
 
 if [ ! -e labels.txt ]; then
-    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/labels.txt $PWD/
+    cp $home_dir/DeepStream-Yolo/labels.txt $PWD/
 else
     echo "labels.txt already exists!"
 fi
 
 if [ ! -e model_b1_gpu0_fp32.engine ]; then
-    cp $home_dir/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/model_b1_gpu0_fp32.engine $PWD
+    cp $home_dir/DeepStream-Yolo/model_b1_gpu0_fp32.engine $PWD
 else
     echo "model_b1_gpu0_fp32.engine already exists!"
 fi
